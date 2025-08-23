5d23d1f7aaea1f54a42269193ba283d1f9a401c1
Salaheddinetah <salaheddine.tahir@student.ucll.be>
2025-08-23 18:18:38 +0200

    added function showInfo, addRecipe

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 
+
+
+const allRecipes = [{
+    title: 'Zucchuni patties',
+    allergy: 'gv',
+    level: 'Super makkelijk'
+},
+    {
+        title: 'Zucchuni patties',
+        allergy: 'g',
+        level: 'Super makkelijk'
+    }]
+
+function addRecipe(recipe) {
+    const recipeArticle = document.createElement('article');
+    recipeArticle.innerHTML = `<h2>${recipe.title}</h2><h3>${recipe.level}</h3><h3>${recipe.allergy}</h3>`;
+    recipeArticle.classList.add(`${recipe.allergy}`);
+    document.querySelector('.recipe-article').appendChild(recipeArticle);
+}
+
+function recipeNumber() {
+    document.querySelector('.numberRecipes').innerHTML += `${allRecipes.length}`;
+}
+
+function showInfo(){
+    allRecipes.forEach(addRecipe);
+    recipeNumber();
+}
+
+
+showInfo();
