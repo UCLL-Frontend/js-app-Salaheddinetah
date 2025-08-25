7ff7ac0f381d961336af02abac5a59429bfe0a8f
Salaheddinetah <salaheddine.tahir@student.ucll.be>
2025-08-25 20:41:32 +0200

    added image place holder for my basicRecipes. Updated CSS for RWD

--- recipes.js
+++ recipes.js
@@ -1,11 +1,13 @@
 export const basicRecipes = [
     {
+        image: `https://anitalianinmykitchen.com/wp-content/uploads/2015/08/zucchini-patties-image-2-1-of-1.jpg`,
         title: 'Zucchuni patties',
         text: 'Recipe text',
         allergy: 'gv',
         level: 'Super makkelijk'
     },
     {
+        image: `https://images.slurrp.com/prod/recipe_images/transcribe/snacks/Minced-Beef-Patties.webp`,
         title: 'Beef patties',
         text: 'Recipe text',
         allergy: 'lv',
