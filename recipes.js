9fc1c3d87c42c7ed7c18cc86191d322528232743
Salaheddinetah <salaheddine.tahir@student.ucll.be>
2025-08-27 16:24:34 +0200

    added recipe link button, added css for the button, added more recipes

--- recipes.js
+++ recipes.js
@@ -1,5 +1,11 @@
 export const basicRecipes = [
     {
+        image: `https://miljuschka.nl/wp-content/uploads/2020/03/Broccolirijst-met-kikkererwten-en-granaatappel_MF_3.jpg`,
+        title: 'Broccolirijst met granaatappel',
+        link: `https://miljuschka.nl/10-x-suikervrij/`,
+        allergy: 'sv',
+        level: 'Makkelijk'
+    },{
         image: `https://anitalianinmykitchen.com/wp-content/uploads/2015/08/zucchini-patties-image-2-1-of-1.jpg`,
         title: 'Zucchuni patties',
         link: `https://downshiftology.com/recipes/goat-cheese-zucchini-fritters/`,
@@ -12,5 +18,68 @@ export const basicRecipes = [
         link: `https://downshiftology.com/recipes/goat-cheese-zucchini-fritters/`,
         allergy: 'lv',
         level: 'Makkelijk'
+    },
+    {
+        image: `https://miljuschka.nl/wp-content/uploads/2020/03/Broccolirijst-met-kikkererwten-en-granaatappel_MF_3.jpg`,
+        title: 'Broccolirijst met granaatappel',
+        link: `https://miljuschka.nl/10-x-suikervrij/`,
+        allergy: 'sv',
+        level: 'Makkelijk'
+    },
+    {
+        image: `https://miljuschka.nl/wp-content/uploads/2020/03/quiche-zonder-korst_MF_3.jpg`,
+        title: 'Quiche zonder korst',
+        link: `https://miljuschka.nl/10-x-suikervrij/`,
+        allergy: 'sv',
+        level: 'Gemiddeld'
+    },
+    {
+        image: `https://cookinglife.nl/media/magefan_blog/suikervrijebananencake.jpg`,
+        title: 'Suikervrije bananencake',
+        link: `https://cookinglife.nl/pages/foodies/suikervrij`,
+        allergy: 'sv',
+        level: 'Makkelijk'
+    },
+    {
+        image: `https://cookinglife.nl/media/magefan_blog/kokoskoekjeszondersuiker.jpg`,
+        title: 'Kokoskoeken zonder suiker',
+        link: `https://cookinglife.nl/pages/foodies/suikervrij`,
+        allergy: 'sv,gv',
+        level: 'Super makkelijk'
+    },
+    {
+        image: `https://cookinglife.nl/media/magefan_blog/gezondechocolademuffins.jpg`,
+        title: 'Chocolate muffins met banaan',
+        link: `https://cookinglife.nl/pages/foodies/suikervrij`,
+        allergy: 'gv,lv,sv',
+        level: 'Makkelijk'
+    },
+    {
+        image: `https://static.ah.nl/static/recepten/img_013610_890x594_JPG.jpg`,
+        title: 'Smoothiebowl met bosvruchten',
+        link: `https://www.jumbo.com/recepten/dieet/lactosevrij`,
+        allergy: 'lv',
+        level: 'Super makkelijk'
+    },
+    {
+        image: `https://static.ah.nl/static/recepten/img_013605_890x594_JPG.jpg`,
+        title: 'Miniquiches met spruiten en wortel',
+        link: `https://www.jumbo.com/recepten/dieet/lactosevrij`,
+        allergy: 'lv',
+        level: 'Gemiddeld'
+    },
+    {
+        image: `https://www.jumbo.com/dam/jcr:66a9e872-28a1-426b-bbcb-881491a2534d/750x750`,
+        title: 'Lactosevrije yoghurt-panna cotta',
+        link: `https://www.jumbo.com/recepten/dieet/lactosevrij`,
+        allergy: 'lv',
+        level: 'Gemiddeld'
+    },
+    {
+        image: `https://lactosevrijzijn.nl/wp-content/uploads/2021/04/lactosevrije-bananencake.jpg`,
+        title: 'Bananencake met dadels en walnoten',
+        link: `https://lactosevrijzijn.nl/lactosevrije-recepten/`,
+        allergy: 'lv,sv',
+        level: 'Beetje Moeilijk'
     }
 ];
