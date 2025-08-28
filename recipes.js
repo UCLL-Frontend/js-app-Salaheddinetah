bc2c434005a23005db907a62c391f3984ce44f45
Salaheddinetah <salaheddine.tahir@student.ucll.be>
2025-08-28 08:20:26 +0200

    added more recipes

--- recipes.js
+++ recipes.js
@@ -26,20 +26,60 @@ export const basicRecipes = [
         link: `https://njam.tv/recepten/quiche-zonder-korst`,
         allergy: 'sv',
         level: 'Gemiddeld'
-    }, {
+    },
+    {
         image: `https://static.ah.nl/static/recepten/img_090111_612x450_JPG.jpg`,
         title: `Hamburger broodjes`,
         link: `https://www.ah.nl/allerhande/recept/R-R1188628/glutenvrije-hamburgerbroodjes`,
         allergy: 'lv,gv',
         level: 'Gemiddeld'
-    } , {
+    },
+    {
         image: `https://rutgerbakt.nl/wp-content/uploads/2020/05/gezond_bananenbrood-scaled-1200x0-c-default-375x0-c-default.webp`,
         title: `Gezonde bananenbrood `,
         link: `https://rutgerbakt.nl/gezond-bakken-recepten/gezond-bananenbrood-met-havermout-recept/`,
         allergy: 'gv, lv, sv',
         level: 'Makkelijk'
+    },
+    {
+        image: `https://static.ah.nl/static/recepten/img_RAM_PRD208678_1224x900_JPG.jpg`,
+        title: 'Ananassalsa met kiwi, jalapeno en koriander',
+        link: `https://www.ah.nl/allerhande/recept/R-R1201510/ananassalsa-met-kiwi-jalapeno-en-koriander`,
+        allergy: 'lv,gv',
+        level: 'Moeilijk'
+    },
+    {
+        image: `https://static.ah.nl/static/recepten/img_085114_612x450_JPG.jpg`,
+        title: `Zoete aardappel met blauwe-kaassaus en kip`,
+        link: `https://www.ah.nl/allerhande/recept/R-R1187806/zoete-aardappel-met-blauwe-kaassaus-en-kip`,
+        allergy: 'gv',
+        level: 'Makkelijk'
+    },
+    {
+        image: `https://static.ah.nl/static/recepten/img_087405_1224x900_JPG.jpg`,
+        title: `Geroosterde bloemkool met harissa`,
+        link: `https://www.ah.nl/allerhande/recept/R-R1189490/gehele-geroosterde-bloemkool-met-harissa-en-munt`,
+        allergy: 'gv,lv',
+        level: 'Makkelijk'
+    },  {
+        image: `https://static-images.jumbo.com/product_images/Recipe_503595-01_560x560_2.jpg`,
+        title: `Zalm met spinazie en tagliatelle`,
+        link: `https://www.jumbo.com/recepten/zalm-met-spinazie-en-tagliatelle-1018698-1`,
+        allergy: 'gv, lv',
+        level: 'Gemiddeld'
+    },
+    {
+        image: `https://liefdevoorlekkers.nl/wp-content/uploads/2018/03/Zoete-aardappelsoep-e1521818848836.jpg`,
+        title: `Zoete aardappelsoep met kokosmelk`,
+        link: `https://www.ah.nl/allerhande/recept/R-R1187806/zoete-aardappel-met-blauwe-kaassaus-en-kip`,
+        allergy: 'gv',
+        level: 'Makkelijk'
+    },  {
+        image: `https://i0.wp.com/jennyalvares.com/wp-content/uploads/2020/05/currrry.jpg?resize=512%2C768&ssl=1`,
+        title: `Vegetarische curry met kikkererwten`,
+        link: `https://www.eatme.eu/nl/recepten/vegetarische-curry-met-kikkererwten`,
+        allergy: 'gv, lv',
+        level: 'Makkelijk'
     }
 
-
 ];
-
